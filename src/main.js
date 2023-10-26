d7494389701988320818fb6e2d510a0a26593dee
Zavac <2014377205@qqcom>
2023-10-26 14:09:18 +0800

    安装项目依赖

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+const app = createApp(App)
+// element css
+import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
 
-createApp(App).mount('#app')
+
+app.use(ElementPlus)
+app.mount('#app')
